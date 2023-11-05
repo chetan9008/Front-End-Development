1a0e82e2d94077e54d58e6194f4028b6adaf3344
chetan <chetankumar70174577@gmail.com>
2023-11-05 12:27:17 +0530

    my project is completed

---
+++ Bootstrap Project/js/main.js
@@ -0,0 +1,6 @@
+const lightbox = GLightbox({
+    'href': 'https://www.youtube.com/watch?v=iJKCj8uAHz8',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+});
